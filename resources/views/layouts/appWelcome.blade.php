8134c3009e79aeba666eb8533170c3370bc1a7f4
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-07-03 19:57:20 +0700

    เปลี่ยน class

--- resources/views/layouts/appWelcome.blade.php
+++ resources/views/layouts/appWelcome.blade.php
@@ -25,7 +25,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
+
+
+
 
     <title>mikupost</title>
 </head>
