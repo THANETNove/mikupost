f613787caa108af46be4bc425a9feac6b2ebd6b8
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-07-09 23:01:11 +0700

    add

--- resources/views/admin/auth/index.blade.php
+++ resources/views/admin/auth/index.blade.php
@@ -70,29 +70,7 @@
     </div>
 
 </div>
-<script>
-const copyBtns = document.querySelectorAll('.copy-btn');
-copyBtns.forEach(copyBtn => {
-    copyBtn.addEventListener('click', () => {
-        const textToCopy = copyBtn.dataset.text;
-        const tempInput = document.createElement('input');
-        tempInput.value = textToCopy;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand('copy');
-        document.body.removeChild(tempInput);
-        /*   alert('คัดลอกข้อความสำเร็จ'); */
-        var paragraph = document.getElementById("copy-message");
-        var text = document.createTextNode("คัดลอกข้อความสำเร็จ");
-        paragraph.appendChild(text);
 
-        // ซ่อน Alert หลังจาก 3 วินาที (3000 มิลลิวินาที)
-        setTimeout(() => {
-            var textNode = paragraph.firstChild;
-            paragraph.removeChild(textNode);
-        }, 1000);
-    });
-});
-</script>
+@include('admin.auth.copyJs')
 
 @endsection
